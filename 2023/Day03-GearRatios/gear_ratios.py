a5db4c09778a08f8176c9bec075c17b69cc78622
CodeBoy-source <briansena23@hotmail.com>
2023-12-09 17:40:50 +0100

    feature (GearRatio): Sum all the numbers adyancent to a symbol in a table

---
+++ 2023/Day03-GearRatios/gear_ratios.py
@@ -0,0 +1,66 @@
+"""
+[SUMMARY]
+You need to use a gondola lift... it's broken.
+You offer to help. There seems to be a part missing.
+If you can add up all the part numbers in the engine schematic,
+it should be easy to work out which part is missing.
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+Apparently any number adjacent to a symbol, even diagonally,
+is a "part number" and should be included in your sum (4361)
+"""
+import re
+import argparse
+from utils import check_around, check_left, check_right, check_vertical
+
+
+def sum_symbols(lines: list[str]):
+    symbol_expr = re.compile('[^0-9.]')
+    first_line_matches = re.finditer(symbol_expr, lines[0])
+    value = 0
+    # Edge case 1: The first line only checks horizontaly and below.
+    for match in first_line_matches:
+        left, lines[0] = check_left(match.start(), lines[0])
+        right, lines[0] = check_right(match.start(), lines[0])
+        below, lines[1] = check_vertical(match.start(), lines[1])
+        value += left + right + below
+    # All the other methods check_around them
+    for i in range(1, len(lines)-1):
+        line = lines[i]
+        matches = re.finditer(symbol_expr, line)
+        for match in matches:
+            around, lines[i-1:i+2] = check_around(match.start(),
+                                                  lines[i-1:i+2])
+            value += around
+    # Edge case 2: The last line only checks horizontaly and above.
+    last_line_matches = re.finditer(symbol_expr, lines[-1])
+    for match in last_line_matches:
+        left, lines[-1] = check_left(match.start(), lines[-1])
+        right, lines[-1] = check_right(match.start(), lines[-1])
+        above, lines[-2] = check_vertical(match.start(), lines[-2])
+        value += left + right + above
+    return value
+
+
+def read_file(filename: str):
+    with open(input_args.input, 'r') as file:
+        lines = file.readlines()
+    lines = [x.strip('\n') for x in lines]
+    return lines
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', type=str,
+                        default='input.txt')
+    input_args = parser.parse_args()
+    lines = read_file(input_args.input)
+    print(sum_symbols(lines))
---
+++ 2023/Day03-GearRatios/utils.py
@@ -0,0 +1,55 @@
+def check_horizontal(direction: int, match: int, line: str):
+    value = 0
+    expanding = ''
+    newline = list(line)
+    while match > 0 and match < len(line) - 1\
+            and line[match + direction].isdigit():
+        expanding += line[match + direction]
+        newline[match + direction] = '.'
+        match += direction
+    if expanding != '':
+        value = int(expanding[::direction])
+    return value, "".join(newline)
+
+
+def check_vertical(match: int, line: str):
+    value = 0
+    expanding = ''
+    newline = list(line)
+    if line[match].isdigit():
+        while match >= 1 and line[match - 1].isdigit():
+            match -= 1
+        while match < len(line) and line[match].isdigit():
+            expanding += line[match]
+            newline[match] = '.'
+            match += 1
+        if expanding != '':
+            value = int(expanding)
+    return value, "".join(newline)
+
+
+def check_left(match: int, line: str):
+    return check_horizontal(-1, match, line)
+
+
+def check_right(match: int, line: str):
+    return check_horizontal(1, match, line)
+
+
+def check_around(match: int, lines: list[str]):
+    line_above, line_actual, line_below = lines
+    left, line_actual = check_left(match, line_actual)
+    right, line_actual = check_right(match, line_actual)
+    above, line_above = check_vertical(match, line_above)
+    la_diag = ra_diag = 0
+    if not above:
+        la_diag, line_above = check_left(match, line_above)
+        ra_diag, line_above = check_right(match, line_above)
+    below, line_below = check_vertical(match, line_below)
+    lb_diag = rb_diag = 0
+    if not below:
+        lb_diag, line_below = check_left(match, line_below)
+        rb_diag, line_below = check_right(match, line_below)
+    value = left + right + above + la_diag \
+        + ra_diag + below + lb_diag + rb_diag
+    return value, [line_above, line_actual, line_below]

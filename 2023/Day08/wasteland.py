9afd44153a6fd916975c6fe1e77c304083607c60
CodeBoy-source <briansena23@hotmail.com>
2024-05-01 19:51:43 +0200

    feat(Day07): Part 2 using lcm reduce

--- 2023/Day08/wasteland.py
+++ 2023/Day08/wasteland.py
@@ -3,10 +3,13 @@ from __future__ import annotations
 import argparse
 import re
 
+import numpy as np
+
 
 def read_file(input_file, part_two):
     hash_map = {}
     instructions = []
+    start_nodes = []
     with open(input_file) as file:
         instructions_str = file.readline()
         instructions = [
@@ -21,9 +24,11 @@ def read_file(input_file, part_two):
             line = re.sub(r"\s+", " ", line)
             values = line.strip().split(" ")
             key, left, right = values
+            if key.endswith("A"):
+                start_nodes += [key]
             hash_map[key] = [left, right]
 
-    return instructions, hash_map
+    return instructions, hash_map, start_nodes
 
 
 def transverse_map(instructions, hash_map):
@@ -38,11 +43,39 @@ def transverse_map(instructions, hash_map):
     return steps
 
 
+def transverse_all_maps(instructions, hash_map, start_nodes):
+    def _transverse_map(start_node):
+        _index = 0
+        _steps = 1
+        location = hash_map[start_node][instructions[_index]]
+        while not location.endswith("Z"):
+            _index += 1
+            _steps += 1
+            location = hash_map[location][instructions[_index % len(instructions)]]
+
+        return _steps
+
+    steps = 1
+    steps_per_start = []
+    for start in start_nodes:
+        steps_per_start.append(_transverse_map(start))
+
+    steps = np.lcm.reduce(np.asarray(steps_per_start))
+    print(steps_per_start)
+    return steps
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", type=str, default="input.txt")
     parser.add_argument("--part-two", action="store_true")
     input_args = parser.parse_args()
-    instructions, hash_map = read_file(input_args.input, input_args.part_two)
-    steps = transverse_map(instructions, hash_map)
+    instructions, hash_map, start_nodes = read_file(
+        input_args.input,
+        input_args.part_two,
+    )
+    if not input_args.part_two:
+        steps = transverse_map(instructions, hash_map)
+    else:
+        steps = transverse_all_maps(instructions, hash_map, start_nodes)
     print(steps)

260aee5c394e894afb35d92cd65093d4e8b17e95
CodeBoy-source <briansena23@hotmail.com>
2024-05-01 18:45:48 +0200

    feat(Day07): add part two code
    Select J type before asserting the points in our class

--- 2023/Day07-CamelCards/camelcards.py
+++ 2023/Day07-CamelCards/camelcards.py
@@ -5,18 +5,31 @@ import re
 
 
 RANKS = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+PART_TWO_RANKS = ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"]
 
 
 class Hand:
-    def __init__(self, hand: str, bid: int):
+    def __init__(self, hand: str, bid: int, part_two: bool = False):
         self.hand = hand
         self.bid = bid
-        self.points = self.rank()
+        self.points = self.rank(part_two)
 
-    def rank(self):
+    def rank(self, part_two: bool = False):
         points = ""
-        for card in set(self.hand):
-            points += str(len(re.findall(card, self.hand)) - 1)
+        hand_set = set(self.hand)
+        deck = self.hand
+        if part_two and "J" in hand_set:
+            j_is = ""
+            max_ = 0
+            for card in hand_set:
+                matches = len(re.findall(card, self.hand.replace("J", card)))
+                if matches > max_:
+                    j_is = card
+                    max_ = matches
+            deck = self.hand.replace("J", j_is)
+            print(self.hand, deck, j_is)
+        for card in set(deck):
+            points += str(len(re.findall(card, deck)) - 1)
         points = points.replace("0", "")
         if points == "12":
             points = "21"
@@ -45,12 +58,12 @@ class Hand:
         return f"{self.hand} {self.points} {self.bid}"
 
 
-def read_file(input_file):
+def read_file(input_file, part_two: bool = False):
     hands = []
     with open(input_file) as file:
         while line := file.readline():
             hand, bid = line.strip("\n").split(" ")
-            hands.append(Hand(hand, int(bid)))
+            hands.append(Hand(hand, int(bid), part_two))
     hands = sorted(hands)
     return hands
 
@@ -58,8 +71,11 @@ def read_file(input_file):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", type=str, default="input.txt")
+    parser.add_argument("--part-two", action="store_true")
     input_args = parser.parse_args()
-    hands = read_file(input_args.input)
+    if input_args.part_two:
+        RANKS = PART_TWO_RANKS
+    hands = read_file(input_args.input, input_args.part_two)
     ranks = []
     for i, hand in enumerate(hands):
         ranks.append(hand.bid * (i + 1))

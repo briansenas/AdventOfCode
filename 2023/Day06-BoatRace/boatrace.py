4a47b2116c2ecae157ab5ebd671b112d1207b7a4
CodeBoy-source <briansena23@hotmail.com>
2024-05-01 17:31:12 +0200

    perf(Day06): implement binary search for segments
    This improves part-two performance

--- 2023/Day06-BoatRace/boatrace.py
+++ 2023/Day06-BoatRace/boatrace.py
@@ -28,9 +28,64 @@ def get_total_wins_bruteforce(times, distances):
         total_speed = 1
         for t in range(1, time):
             remaining = time - t
-            if remaining * total_speed > distance:
+            if is_win(remaining, distance, total_speed):
                 total_wins[i] += 1
             total_speed += 1
+    return reduce(lambda x, y: x * y, total_wins)
+
+
+def is_win(remaining, distance, speed):
+    return remaining * speed > distance
+
+
+def get_lower_bound(time, distance):
+    min_ = 0
+    max_ = time
+    mid = (max_ + min_) // 2
+    while max_ - min_ > 1:
+        remaining = time - mid
+        if is_win(remaining, distance, mid):
+            # If we are still winning keep searching lower
+            max_ = mid
+        else:
+            # We have passed it therefore we need to search higher
+            min_ = mid
+        mid = (max_ + min_) // 2
+    left = is_win(time - mid - 1, distance, mid - 1)
+    right = is_win(time - mid + 1, distance, mid + 1)
+    if left and not right:
+        return mid - 1
+    else:
+        return mid + 1
+
+
+def get_higher_bound(time, distance):
+    min_ = 0
+    max_ = time
+    mid = (max_ + min_) // 2
+    while max_ - min_ > 1:
+        remaining = time - mid
+        if is_win(remaining, distance, mid):
+            min_ = mid
+        else:
+            max_ = mid
+        mid = (max_ + min_) // 2
+    left = is_win(time - mid - 1, distance, mid - 1)
+    right = is_win(time - mid + 1, distance, mid + 1)
+    if left and not right:
+        return mid - 1
+    else:
+        return mid + 1
+
+
+def get_total_wins_binarysearch(times, distances):
+    total_wins = [0 for _ in range(0, len(times))]
+    for i, pairs in enumerate(zip(times, distances)):
+        time, distance = pairs
+        low = get_lower_bound(time, distance)
+        high = get_higher_bound(time, distance)
+        total_wins[i] = high - low
+
     return reduce(lambda x, y: x * y, total_wins)
 
 
@@ -38,8 +93,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", type=str, default="input.txt")
     parser.add_argument("--part-two", action="store_true")
+    parser.add_argument("--bruteforce", action="store_true")
     input_args = parser.parse_args()
     time, distance = read_file(input_args.input, input_args.part_two)
     print(time, distance)
-    total_wins = get_total_wins_bruteforce(time, distance)
+    if input_args.bruteforce:
+        total_wins = get_total_wins_bruteforce(time, distance)
+    else:
+        total_wins = get_total_wins_binarysearch(time, distance)
     print(total_wins)
